6d512ed204bca81efe1f051bf657510d7df1084b
Dawid Andrzejewski <mrceleius@gmail.com>
2024-03-14 14:50:09 +0100

    initial commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Basic Cross-Platform Flowmodoro App</h1>
+      <img src={icon} alt="icon" />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
